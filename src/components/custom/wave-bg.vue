221ec8b05ae2eac4b0af99e9e043bd3605e122ef
KingPrimes <Sakitama_q@163.com>
2025-01-30 03:35:19 +0800

    feat: 删除旧的构建工作流，添加动态获取必应壁纸功能

--- src/components/custom/wave-bg.vue
+++ src/components/custom/wave-bg.vue
@@ -1,12 +1,47 @@
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
 defineOptions({ name: 'WaveBg' });
+
+interface image {
+  startdate: string;
+  fullstartdate: string;
+  enddate: string;
+  url: string;
+  urlbase: string;
+  copyright: string;
+  copyrightlink: string;
+  title: string;
+}
+
+interface BingImage {
+  images: image[];
+}
+
+const imageUrl = ref('');
+
+async function getBingImage() {
+  await fetch('https://raw.onmicrosoft.cn/Bing-Wallpaper-Action/main/data/zh-CN_update.json', {
+    method: 'GET',
+    redirect: 'follow'
+  }).then(response => {
+    response.text().then(data => {
+      const bingImage: BingImage = JSON.parse(data);
+      const random = Math.floor(Math.random() * bingImage.images.length);
+      imageUrl.value = `https://cn.bing.com${bingImage.images[random].url}`;
+    });
+  });
+}
+onMounted(() => {
+  getBingImage();
+});
 </script>
 
 <template>
   <div
     class="absolute-lt z-1 size-full overflow-hidden"
     :style="{
-      backgroundImage: `url(https://bing.img.run/uhd.php)`,
+      backgroundImage: `url(${imageUrl})`,
       backgroundSize: `cover`
     }"
   ></div>
